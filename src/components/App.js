09e1bfada6900b42d8afc703b57fa8e4163d1777
Rahul <rahulvaidya148@gmail.com>
2022-05-02 14:39:32 +0530

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,46 @@ import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='main'>
-        <div className='tabs'>
-          <div className='tab'>Movie</div>
-            <div className='tab'>Favourites</div>
-      </div>
-      <div className='list'>
-        {data.map(movie =>(
-          <MovieCard movie={movie} />
+class App extends React.Component {
 
-        ))}
+  componentDidMount(){
+   const { store } = this.props;
+   store.subscribe(() =>{
+     console.log('UPDATED');
+    this.forceUpdate();
+   });
 
+    // make api call
+    // dispatch action
+    store.dispatch({
+      type : 'ADD_MOVIES',
+      movies : data
+    });
+    console.log('STATE',this.props.store.getState());
+  }
+
+  render(){
+  console.log('RENDER')
+  const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className='main'>
+          <div className='tabs'>
+            <div className='tab'>Movie</div>
+              <div className='tab'>Favourites</div>
+        </div>
+        <div className='list'>
+          {movies.map((movie , index) =>(
+            <MovieCard movie={movie}  key = {`movies-${index}`} />
+
+          ))}
+
+        </div>
+        </div>
       </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,14 +10,20 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log('store', store);
-console.log('STATE', store.getState());
+// console.log('BEFORE STATE', store.getState());
 
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Superman'}]
+// });
+
+// console.log('AFTER STATE', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 

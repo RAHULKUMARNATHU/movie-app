15d2caffa0d8ed0376e268dbcab4d981f71b7d59
Rahul <rahulvaidya148@gmail.com>
2022-05-02 15:43:50 +0530

    change state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
-{
-    type : 'ADD_MOVIES' ,
-    movies =  [m1,m2,m3]
+// {
+//     type : 'ADD_MOVIES' ,
+//     movies =  [m1,m2,m3]
+// }
+
+// action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies(movies){
+    return{
+        type:ADD_MOVIES,
+        movies
+    }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 
 class App extends React.Component {
@@ -15,16 +16,13 @@ class App extends React.Component {
 
     // make api call
     // dispatch action
-    store.dispatch({
-      type : 'ADD_MOVIES',
-      movies : data
-    });
+    store.dispatch(addMovies(data));
     console.log('STATE',this.props.store.getState());
   }
 
   render(){
-  console.log('RENDER')
-  const movies = this.props.store.getState();
+  console.log('RENDER'  , this.props.store.getState())
+  const {list } = this.props.store.getState(); //{list:[] , favourites:[]}
     return (
       <div className="App">
         <Navbar />
@@ -34,7 +32,7 @@ class App extends React.Component {
               <div className='tab'>Favourites</div>
         </div>
         <div className='list'>
-          {movies.map((movie , index) =>(
+          {list.map((movie , index) =>(
             <MovieCard movie={movie}  key = {`movies-${index}`} />
 
           ))}

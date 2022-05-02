e90e765cf3db2d06a13bb4b9d6ba73c51013918b
Rahul <rahulvaidya148@gmail.com>
2022-05-02 06:11:30 +0530

    Created :: Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('STATE', store.getState());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
